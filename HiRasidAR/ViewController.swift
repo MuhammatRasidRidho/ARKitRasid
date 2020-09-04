1c2adc6cca667f53110352070583e5143e325fcc
Mas Rasid <rasid517@gmail.com>
2020-09-04 09:23:56 +0700

    Add Screen shoot

--- HiRasidAR/ViewController.swift
+++ HiRasidAR/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // sceneView.scene = scene
         
         
-        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin,
+        sceneView.debugOptions = [	
                                   ARSCNDebugOptions.showFeaturePoints]
         sceneView.autoenablesDefaultLighting = true
         
